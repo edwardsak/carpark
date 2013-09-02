b571402edb5aae2e5969903070852aa376298eb2
edwardsak <seichek@gmail.com>
2013-09-02 10:55:20 +0800

    structuring 2

--- controllers/home.py
+++ controllers/home.py
@@ -1,9 +1,5 @@
-from models.models import User
-
 import webapp2
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        users = User.query().fetch()
-        
         self.response.write("Hello world!")
